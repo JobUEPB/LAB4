3915c833447573a192e0da84d07546fcabd9ee5a
usuario <usuario@DESKTOP-8LIEST6>
2017-02-09 19:19:01 -0300

    Primeiras Classes

---
+++ src/entidades/Jogador.java
@@ -0,0 +1,52 @@
+package entidades;
+
+public class Jogador {
+	
+	private String nome;
+	private int escolha_zero_um;
+	private int escolha_par_impar;
+	private boolean capitao = false;
+	private int time;
+	
+	
+	public Jogador(String nome){
+		
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getEscolha_zero_um() {
+		return escolha_zero_um;
+	}
+	public void setEscolha_zero_um(int escolha_zero_um) {
+		this.escolha_zero_um = escolha_zero_um;
+	}
+	public int getEscolha_par_impar() {
+		return escolha_par_impar;
+	}
+	public void setEscolha_par_impar(int escolha_par_impar) {
+		this.escolha_par_impar = escolha_par_impar;
+	}
+	public boolean isCapitao() {
+		return capitao;
+	}
+	public void setCapitao(boolean capitao) {
+		this.capitao = capitao;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	
+	
+	
+
+}
--- src/entidades/Main.java
+++ src/entidades/Main.java
@@ -3,7 +3,13 @@ package entidades;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		Jogador jogador1 = new Jogador("Job");
+		Jogador jogador2 = new Jogador("Joao");
+		Jogador jogador3 = new Jogador("Pedra");
+		
+
+		System.out.println(" ");
 
 	}
 
